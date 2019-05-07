7dadefbca38bd1a1f938c2f7af93ad098d50dcac
maxwellbenton <maxwellbenton@gmail.com>
2019-05-07 12:02:21 -0400

    adds solution

--- answers.rb
+++ answers.rb
@@ -4,31 +4,33 @@ require_relative "./lib/card.rb"
 require_relative "./lib/table.rb"
 
 def name_of_the_game
-  # Based on the rules, what game are you playing?
-  # Return its name, as a string, from this method.
+# Return the name of the game as a String
+"war"
 end
 
 def what_code_is_required_to_play_the_game
-  # Type the code you would use to start the game.
-  # This should be a Ruby expression
+# Return the single expression needed to start the game
+Game.new()
 end
 
 def which_method_gets_keyed_user_input
-  # Return a String that is the name of the instance method that gets user input
+# Return the String of name of the instance method that gets user input
+"get_player_names"
 end
 
 def which_methods_only_handle_display
-  # Find the method names for all instance methods that directly output content.
-  # Return an these methods' names as an Array of Strings
+# Return an Array of String method names for all instance methods that strictly handle outputing content
+["display_current_scores", "announce_winner", "display_turn_results", "display_turn_winner", "display_tie"]
 end
 
 
 def which_method_creates_cards
-  # Return a String that is the name of the instance method that returns cards as a 'deck'
-
+# Return the String name of the instance method that returns cards as a 'deck'
+"build_deck"
 end
 
 def double_deck_game
-  # Suppose we wanted to use two decks of cards
-  # Return as a String the name of the method that we would modify to play a double deck game
+# Lets say we wanted to use two decks of cards
+# Return the String name of the method that we would modify to play a double deck game
+"initialize"
 end
